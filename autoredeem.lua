087ea5042ea438a0799e52225d497685ce74792a
UltimateDuper <104033466+PoloISFAT@users.noreply.github.com>
2022-11-12 23:16:09 +0100

    Create autoredeem.lua

---
+++ autoredeem.lua
@@ -0,0 +1,12 @@
+_G.toggle = true
+while _G.toggle do
+wait()
+  
+spawn(function()
+if game:GetService("Workspace")["__MAP"].Interactive["Rank Rewards"].Pad.BillboardGui.Timer.Text == "Redeem!" then
+while wait() do
+local Library = require(game:GetService("ReplicatedStorage").Framework.Library)
+Library.Network.Invoke("redeem rank rewards", {})
+end
+end
+end)
