5f6f6ce4947e04b6d6fa2353b377512fc57fb7b3
UltimateDuper <104033466+PoloISFAT@users.noreply.github.com>
2022-11-12 17:52:37 +0100

    Create gui.lua

---
+++ gui.lua
@@ -0,0 +1,25 @@
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Fuze Hub", "DarkTheme")
+
+local AutoFarm = Window:NewTab("AutoFarm")
+local AutoFarmSection = AutoFarm:NewSection("AutoFarm")
+
+AutoFarmSection:NewToggle("Auto Collect orbs", "Teleport all the orbs to you", function(state)
+    if state then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/PoloISFAT/FuzeHub/main/autocollect?token=GHSAT0AAAAAAB3CE227ZQHN44Q25VZPKKXWY3PZUKA"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/PoloISFAT/FuzeHub/main/disableautocollect?token=GHSAT0AAAAAAB3CE226JOK3L2JJRELUZEQMY3PZU5Q"))()
+    end
+end)
+
+local Misc = Window:NewTab("Misc")
+local Misc = Misc:NewSection("Misc")
+
+Misc:NewSlider("Speed", "Change Player Walkspeed", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+Misc:NewSlider("Jump", "Change Player JumpPower", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
