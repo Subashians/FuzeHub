02cc2a81c7c9f63e26a708643daf6294b5a9d010
Subashians <104033466+Subashians@users.noreply.github.com>
2024-04-21 12:42:18 +0200

    Update fakebot.lua

--- fakebot.lua
+++ fakebot.lua
@@ -1,14 +1,7 @@
-if _G.ideez:sub(1, 8) == "rbxid://" then
-    -- Extract the id part after "rbxid://"
-    local _G.id = rbxIdString:sub(9)
-    return _G.id
-else
-     -- If rbxIdString doesn't start with "rbxid://", return nil
-     return nil
-end
-
+funnid = _G.rbxid
+Asset = tonumber(string.match(funnid, "%d+"))
 local Size = "420x420"
 
-local Image = game:HttpGet("https://thumbnails.roblox.com/v1/assets?assetIds=".._G.id.."&returnPolicy=PlaceHolder&size="..Size.."&format=png")
+local Image = game:HttpGet("https://thumbnails.roblox.com/v1/assets?assetIds="..Asset.."&returnPolicy=PlaceHolder&size="..Size.."&format=png")
 
 _G.thumbnail = game.HttpService:JSONDecode(Image).data[1].imageUrl
