64ab20e241d27809ec72f34c9fad4a139135819d
Subashians <104033466+Subashians@users.noreply.github.com>
2024-04-26 10:38:37 +0200

    Create getthumbnail.lua

---
+++ getthumbnail.lua
@@ -0,0 +1,7 @@
+funnid = _G.image
+Asset = tonumber(string.match(funnid, "%d+"))
+local Size = "420x420"
+
+local Image = game:HttpGet("https://thumbnails.roblox.com/v1/assets?assetIds="..Asset.."&returnPolicy=PlaceHolder&size="..Size.."&format=png")
+
+_G.thumbnail = game.HttpService:JSONDecode(Image).data[1].imageUrl
