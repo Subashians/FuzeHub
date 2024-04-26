539526d1b0299fdebfd46863b1f1c86c696c4e23
Subashians <104033466+Subashians@users.noreply.github.com>
2024-04-26 15:44:52 +0200

    Update getthumbnail.lua

--- getthumbnail.lua
+++ getthumbnail.lua
@@ -8,3 +8,4 @@ local Size = "420x420"
 local Image = game:HttpGet("https://thumbnails.roblox.com/v1/assets?assetIds="..Asset.."&returnPolicy=PlaceHolder&size="..Size.."&format=png")
 
 _G.thumbnail = game.HttpService:JSONDecode(Image).data[1].imageUrl
+print(_G.thumbnail)
